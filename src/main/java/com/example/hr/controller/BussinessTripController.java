846bba9d0f6762f66b96f0260eea97966eed855f
BruceDbigmowang <2483498610@qq.com>
2020-04-07 09:50:40 +0800

    修复出差申请出现负数的错误，并清除调试使用的System.out

--- src/main/java/com/example/hr/controller/BussinessTripController.java
+++ src/main/java/com/example/hr/controller/BussinessTripController.java
@@ -46,7 +46,6 @@ public class BussinessTripController {
             //判断开始时间早于结束时间
             return "请假开始时间应早于请假结束时间";
         }else{
-            System.out.println("执行到此处"+2);
             int year1 = bdate.getYear();
             int year2 = edate.getYear();
             //限制请假时间不可超过1年
@@ -54,14 +53,12 @@ public class BussinessTripController {
                 return "请假不可超过一年";
             }else{
 
-                System.out.println("执行到此处"+4);
                 int month1 = bdate.getMonth();
                 int month2 = edate.getMonth();
                 //限制请假时间不能超过一个月
                 if(month2-month1 > 0){
                     return "请假不可超过一个月";
                 }else{
-                    System.out.println("执行到此处"+5);
                     int day1 = bdate.getDate();
                     int day2 = edate.getDate();
                     int days = day2-day1;
@@ -76,9 +73,7 @@ public class BussinessTripController {
                     }
                     if(days == 0){
                         duration = hour2-hour1;
-                        System.out.println("执行到此处"+2);
                     }else{
-                        System.out.println("执行到此处"+1);
                         if(hour2 < 9){
                             return "9点之前的时间不在请假范围之内";
                         }
@@ -92,23 +87,19 @@ public class BussinessTripController {
                             }else{
                                 targetDay = yearMonth+"-"+s;
                             }
-                            System.out.println(targetDay);
                             LocalDate nowDate = LocalDate.parse(targetDay);
                             int flag1 = nowDate.getDayOfWeek().getValue();
                             if(flag1!=6 && flag1!=7){
                                if(s == day2){
                                    duration = duration+hour2-9;
-                                   System.out.println(duration+"->");
                                }else{
 
                                    duration = duration+8;
-                                   System.out.println(duration+"->>");
                                }
                             }
                         }
 
                     }
-                    System.out.println(duration);
                     String leaveDay =new SimpleDateFormat("yyyy-MM-dd").format(bdate);
                     BussinessTrip bussinessTrip = new BussinessTrip();
                     bussinessTrip.setAccount(account);
